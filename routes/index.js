74021fc4fef450b27ffbb6a3e48598534c9ded8d
Marius Rasch <mras@informatik.uni-kiel.de>
2016-01-13 15:00:18 +0100

    parse url

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,9 @@ router.get('/', function(req, res, next) {
   res.render('explore', { title: 'Map' });
 });
 
+router.get('/map/:id', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.render('explore', { title: 'Map', id: req.params.id });
+});
+
 module.exports = router;
